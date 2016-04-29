e609a7d7b171290321eb7f80ea347f6b7b38b505
Francisco Ochoa Soto <Francisco Ochoa Soto>
2016-04-28 22:47:39 -0700

    agregando valvula

--- index.js
+++ index.js
@@ -1,38 +1,56 @@
 var express = require('express');
+var five  = require("johnny-five");
 var app = express();
 var http = require('http').Server(app);
-
 
 var port = process.env.PORT || 3000;
 
 var io = require('socket.io')(http);
 
-var d = new Date();
-var time = d.getTime() / 1000;
 
-  var dataPoint = [ { 
-          time: Date.now() / 1000, 
-          y: 30 }
-        ]
-        
+var dataPoint = [ { 
+        time: Date.now() / 1000, 
+        y: 0 }
+      ]
+  
+var board = new five.Board({port : 'COM3'});
+
+var temp = 0;
+
+board.on("ready", function() {
+  
+// var virtual = new five.Board.Virtual(
+//     new five.Expander("MCP23017")
+//   );
+  
+
+ var temperature = new five.Thermometer({
+    controller: "LM35",
+    pin: "A0"
+  });
+
+  temperature.on("data", function() {
+    console.log(this.celsius + "°C");
+    temp = this.celsius;
+  });
+  
+});
+      
 io.on('connection', function(socket){
   
   console.log('a user connected');
   
-  var i = 20;
-  
+
   //Initial emit
   socket.emit("temp", dataPoint);
        
   socket.on('recived', function(){
       
-
-    i++;
     setTimeout(function() {
 
         var newPoint = [{ 
           time: Date.now()/1000, 
-          y: i + 1 
+          y: temp 
         }];
         
         socket.emit("temp", newPoint);
